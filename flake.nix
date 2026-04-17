1033dc756f2649351173b221cdf208cee48e58fc
Davide Palma (ElDavo) <github@davidepalma.it>
2026-04-18 00:08:44 +0200

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "Nix flake for developing and building KeyboxScavenger";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            default = pkgs.stdenvNoCC.mkDerivation {
+              pname = "keyboxscavenger";
+              version = "dev";
+              src = ./.;
+
+              nativeBuildInputs = with pkgs; [
+                bash
+                coreutils
+                findutils
+                gawk
+                gnugrep
+                gnused
+                gnutar
+                gzip
+                zip
+              ];
+
+              dontConfigure = true;
+              dontFixup = true;
+
+              buildPhase = ''
+                runHook preBuild
+                export HOME="$TMPDIR"
+                chmod -R u+w .
+                sh ./build.sh
+                runHook postBuild
+              '';
+
+              installPhase = ''
+                runHook preInstall
+                mkdir -p "$out"
+                cp -a _builds "$out/"
+                cp -a module.prop module.json "$out/"
+                runHook postInstall
+              '';
+            };
+          });
+
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                bash
+                coreutils
+                findutils
+                gawk
+                gnugrep
+                gnused
+                gnutar
+                gzip
+                zip
+              ];
+
+              shellHook = ''
+                echo "KeyboxScavenger nix dev shell ready."
+                echo "Try: sh check-syntax.sh && sh build.sh"
+              '';
+            };
+          });
+    };
+}
